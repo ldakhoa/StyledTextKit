f43e679e13ca81788762d5343575e9ca4b8a1e4b
ldakhoa <ldakhoa1308@gmail.com>
2023-01-04 02:13:12 +0700

    Add Example

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -2,18 +2,18 @@
 //  ViewController.swift
 //  Example
 //
-//  Created by Khoa Le on 04/01/2023.
+//  Created by Marcus Wu on 2018/6/14.
+//  Copyright © 2018年 Marcus Wu. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        title = "StyledTextKit Example"
     }
 
-
 }
-
